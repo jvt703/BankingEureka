dab56494b4a511488f29891e7f0edd0b4d01b6d6
Richard <richardfolorunso@protonmail.com>
2023-04-03 13:41:50 -0400

    Eureka server update

--- src/main/java/dev/n1t/EurekaServerApplication.java
+++ src/main/java/dev/n1t/EurekaServerApplication.java
@@ -2,12 +2,15 @@ package dev.n1t;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("EurekaServerApplication Ready.");
 	}
 
 }
